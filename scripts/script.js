1b4c68a77b8fc2a6bbb9f7539e2c51922c5ae6c7
Tengurh <gfama2212@gmail.com>
2025-01-07 20:58:31 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,11 @@
-b
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Bienvenue sur TËNGU RH !");
+    const contactForm = document.getElementById("contact-form");
+    if (contactForm) {
+        contactForm.addEventListener("submit", (e) => {
+            e.preventDefault();
+            alert("Merci de nous avoir contactés ! Nous reviendrons vers vous sous peu.");
+            contactForm.reset();
+        });
+    }
+});
